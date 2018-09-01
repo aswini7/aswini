1af3d25235d667654d11ed8d412d25708a3c7add
aswini7 <madireddyaswini@gmail.com>
2018-09-01 11:45:25 +0530

    helloworld

---
+++ basics/helloworld.c
@@ -0,0 +1,10 @@
+ Volume in drive F is New Volume
+ Volume Serial Number is 828E-547B
+
+ Directory of F:\labs\basics
+
+01-09-2018  11:27    <DIR>          .
+01-09-2018  11:27    <DIR>          ..
+01-09-2018  11:27                 0 helloworld.c
+               1 File(s)              0 bytes
+               2 Dir(s)  249,173,442,560 bytes free
